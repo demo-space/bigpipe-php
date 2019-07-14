925c0deb4594d33619e2b8e0e636887d7bbae6fa
lessfish <lessfish@outlook.com>
2019-07-14 10:07:09 +0800

    传统渲染

---
+++ normal.html.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <style>
+          .box { border: 1px solid red; margin: 10px;}
+        </style>
+    </head>
+    <body>
+        <div class="box">1. <?php echo $var1;?></div>
+        <div class="box">2. <?php echo $var2;?></div>
+        <div class="box">3. <?php echo $var3;?></div>
+    </body>
+</html>
